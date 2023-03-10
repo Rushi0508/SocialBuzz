32e82df0d925e3359aa1c48d0de80e5c404fa8cf
Rushi_0508 <rushigandhi14@gmail.com>
2023-03-10 10:00:58 +0530

    added posts upload

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
 from django.http import HttpResponse
 from django.contrib import messages
-from .models import Profile
+from .models import Profile,Post
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -15,7 +15,18 @@ def index(req):
 
 @login_required(login_url='/signin')
 def upload(req):
-    pass
+    if(req.method=='POST'):
+        user = req.user.username
+        image = req.FILES.get('image_upload')
+        caption = req.POST['caption']
+
+        new_post = Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save();
+        return redirect('/');
+    else:
+        return redirect('/');
+          
+        
 
 @login_required(login_url='/signin')
 def settings(req):
